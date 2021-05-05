de12a8115bd216067d7c71c1517fb28c6c2dbf8b
DominikW <dominik.wukovatz@gmail.com>
2021-05-06 01:09:39 +0200

    Solving of some security issues
    [0] Plain password input
    [1] No frontend hashing
    [2] Outputting important user-related data in the frontend

--- frontend/smart-home/src/app/login/login.component.ts
+++ frontend/smart-home/src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import {HttpService} from '../http-service'
+import { HttpService } from '../http-service'
+import hash from 'object-hash';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,34 +11,28 @@ import {HttpService} from '../http-service'
 })
 export class LoginComponent implements OnInit {
 
-  ngOnInit() : void{
-
+  ngOnInit(): void {
   }
 
-  constructor(private router : Router, private http : HttpService){}
+  constructor(private router: Router, private http: HttpService) { }
 
   loginForm = new FormGroup({
     username: new FormControl(''),
     password: new FormControl('')
-  }); 
+  });
 
-  async submitLogin(){
-    console.log(this.loginForm.get("username").value);
-    console.log(this.loginForm.get("password").value);
-
-    var loginReturn = await this.http.login(this.loginForm.get("username").value, this.loginForm.get("password").value).catch(err => {
+  async submitLogin() {
+    var loginReturn = await this.http.login(this.loginForm.get("username").value, hash(this.loginForm.get("password").value)).catch(err => {
       console.log("error");
     });
-    console.log(loginReturn);
-    if(loginReturn){
+
+    if (loginReturn) {
       localStorage.setItem("token", loginReturn.token);
       localStorage.setItem("id", loginReturn.id);
       localStorage.setItem("isLoggedIn", "true");
       this.router.navigateByUrl("/dashboard/info");
-    }else{
+    } else {
       console.error("failed login");
     }
-
   }
-
 }
